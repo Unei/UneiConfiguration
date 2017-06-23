4fe35675743c0a1114475e8c2b6bee8bbbe76209
au2001 <au2001sfg@gmail.com>
2017-06-24 00:31:03 +0200

    Add javadoc to NavigableFile and IYamlConfiguration interfaces

--- src/main/java/me/unei/configuration/api/IYamlConfiguration.java
+++ src/main/java/me/unei/configuration/api/IYamlConfiguration.java
@@ -2,7 +2,18 @@ package me.unei.configuration.api;
 
 public interface IYamlConfiguration extends IConfiguration {
 
+    /**
+     * Returns this configuration's YAML representation as text.
+     *
+     * @return this configuration's YAML representation
+     */
     public String saveToString();
+
+    /**
+     * Loads the provided YAML data into this YamlConfig.
+     *
+     * @param data The YAML data as text
+     */
     public void loadFromString(String data);
 
 }
--- src/main/java/me/unei/configuration/api/fs/NavigableFile.java
+++ src/main/java/me/unei/configuration/api/fs/NavigableFile.java
@@ -2,9 +2,30 @@ package me.unei.configuration.api.fs;
 
 public interface NavigableFile {
 
+    /**
+     * Returns this navigable file's direct (first) parent,
+     * if it has one, or itself if it doesn't have any parent.
+     *
+     * @return this navigable file's direct parent
+     */
     public NavigableFile getParent();
 
+    /**
+     * Gets this navigable file's root (last parent).
+     * It returns itself if it doesn't have any parent.
+     *
+     * @return this navigable file's root
+     */
     public NavigableFile getRoot();
 
+    /**
+     * Returns the child of this navigable file with the
+     * provided name, or <tt>null</tt> if it doesn't exist
+     * and if it can't be created.
+     *
+     * @param name The name of the child to return
+     *
+     * @return this navigable file's child with the provided name
+     */
     public NavigableFile getChild(String name);
 }
