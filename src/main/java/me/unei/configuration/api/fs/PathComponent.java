c3b8b07beedb31bdcf70cc16c3dd2191b7b6790e
JesFot <jesfot@gmail.com>
2017-07-21 23:08:09 +0200

    Bug Fix - Remove calls to NullArgumentException

--- src/main/java/me/unei/configuration/api/fs/PathComponent.java
+++ src/main/java/me/unei/configuration/api/fs/PathComponent.java
@@ -3,173 +3,175 @@ package me.unei.configuration.api.fs;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
-import org.apache.commons.lang.NullArgumentException;
-
 import me.unei.configuration.api.fs.PathNavigator.PathSymbolsType;
 
 public final class PathComponent {
-
-    private final PathComponentType type;
-    private final String value;
-
-    public PathComponent(PathComponentType type, String value) {
-        this.type = type;
-        this.value = value;
-    }
-
-    public PathComponentType getType() {
-        return this.type;
-    }
-
-    public String getValue() {
-        return this.value;
-    }
-
-    public static String escapeComponent(String component, PathSymbolsType symType) {
-        if (component == null || component.isEmpty()) {
-            return component;
-        }
-
-        boolean absolute = component.startsWith(String.valueOf(symType.root));
-        if (absolute) {
-            component = component.substring(1);
-        }
-
-        component = component.replace(String.valueOf(symType.escape), String.valueOf(new char[]{symType.escape, symType.escape}));
-        component = component.replace(String.valueOf(symType.separator), String.valueOf(new char[]{symType.escape, symType.separator}));
-
-        if (absolute) {
-            component = symType.escape + symType.root + component;
-        }
-        return component;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof PathComponent)) {
-            return false;
-        }
-        PathComponent pc = (PathComponent) other;
-        if (pc.getType().equals(this.getType())) {
-            if (this.getType().equals(PathComponentType.CHILD)) {
-                return this.getValue().contentEquals(pc.getValue());
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public static class PathComponentsList extends ArrayList<PathComponent> {
-
-        private static final long serialVersionUID = 7055238860386957873L;
-
-        private PathSymbolsType symType;
-
-        public PathComponentsList(PathSymbolsType type) {
-            super();
-            this.symType = type;
-        }
-
-        public PathComponentsList(PathComponentsList orig) {
-            super(orig);
-            this.symType = orig.symType;
-        }
-
-        @Override
-        public boolean add(PathComponent element) {
-            if (element == null) {
-                throw new NullArgumentException("element");
-            }
-            return super.add(element);
-        }
-
-        public PathSymbolsType getSymbolsType() {
-            return this.symType;
-        }
-
-        public boolean appendComponent(PathComponentType type, String value) {
-            return this.add(new PathComponent(type, value));
-        }
-
-        public boolean appendChild(String name) {
-            return this.appendComponent(PathComponentType.CHILD, name);
-        }
-
-        public boolean appendRoot() {
-            return this.appendComponent(PathComponentType.ROOT, String.valueOf(symType.root));
-        }
-
-        public boolean appendParent() {
-            return this.appendComponent(PathComponentType.PARENT, symType.parent);
-        }
-
-        public PathComponent last() {
-            if (this.isEmpty()) {
-                return null;
-            }
-            return this.get(this.size() - 1);
-        }
-
-        public String lastChild() {
-            PathComponent last = this.last();
-            if (last != null) {
-                return last.getValue();
-            }
-            return null;
-        }
-
-        public PathComponent removeLast() {
-            if (this.isEmpty()) {
-                return null;
-            }
-            return this.remove(this.size() - 1);
-        }
-
-        /**
-         * @deprecated use {@link PathNavigator#cleanPath(PathComponent.PathComponentsList)} instead
-         */
-        @Deprecated
-        public void cleanPath() {
-            ListIterator<PathComponent> it = this.listIterator();
-            int lr = this.lastIndexOf(new PathComponent(PathComponentType.ROOT, ""));
-            for (int i = 0; i < lr && it.hasNext(); i++) {
-                it.next();
-                it.remove();
-            }
-            it = this.listIterator();
-            while (it.hasNext()) {
-                PathComponent curr = it.next();
-                if (curr.getType().equals(PathComponentType.PARENT)) {
-                    if (it.hasPrevious()) {
-                        PathComponent prev = it.previous();
-                        if (prev.getType().equals(PathComponentType.CHILD)) {
-                            it.remove();
-                            it.next();
-                            it.remove();
-                        }
-                    }
-                }
-            }
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder pathBuilder = new StringBuilder();
-            for (PathComponent component : this) {
-                pathBuilder.append(PathComponent.escapeComponent(component.getValue(), this.symType));
-            }
-            return pathBuilder.toString();
-        }
-
-        @Override
-        public PathComponentsList clone() {
-            PathComponentsList copy = (PathComponentsList) super.clone();
-            copy.symType = this.symType;
-            return copy;
-        }
-    }
-
-    public static enum PathComponentType {
-        ROOT, PARENT, CHILD;
-    }
+	
+	private final PathComponentType type;
+	private final String value;
+	
+	public PathComponent(PathComponentType type, String value) {
+		this.type = type;
+		this.value = value;
+	}
+	
+	public PathComponentType getType() {
+		return this.type;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	
+	public static String escapeComponent(String component, PathSymbolsType symType) {
+		if (component == null || component.isEmpty()) {
+			return component;
+		}
+		
+		boolean absolute = component.startsWith(String.valueOf(symType.root));
+		if (absolute) {
+			component = component.substring(1);
+		}
+		
+		component = component.replace(String.valueOf(symType.escape),
+				String.valueOf(new char[] { symType.escape, symType.escape }));
+		component = component.replace(String.valueOf(symType.separator),
+				String.valueOf(new char[] { symType.escape, symType.separator }));
+		
+		if (absolute) {
+			component = symType.escape + symType.root + component;
+		}
+		return component;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof PathComponent)) {
+			return false;
+		}
+		PathComponent pc = (PathComponent) other;
+		if (pc.getType().equals(this.getType())) {
+			if (this.getType().equals(PathComponentType.CHILD)) {
+				return this.getValue().contentEquals(pc.getValue());
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public static class PathComponentsList extends ArrayList<PathComponent> {
+		
+		private static final long serialVersionUID = 7055238860386957873L;
+		
+		private PathSymbolsType symType;
+		
+		public PathComponentsList(PathSymbolsType type) {
+			super();
+			this.symType = type;
+		}
+		
+		public PathComponentsList(PathComponentsList orig) {
+			super(orig);
+			this.symType = orig.symType;
+		}
+		
+		@Override
+		public boolean add(PathComponent element) {
+			if (element == null) {
+				throw new NullPointerException("element must not be null.");
+			}
+			return super.add(element);
+		}
+		
+		public PathSymbolsType getSymbolsType() {
+			return this.symType;
+		}
+		
+		public boolean appendComponent(PathComponentType type, String value) {
+			return this.add(new PathComponent(type, value));
+		}
+		
+		public boolean appendChild(String name) {
+			return this.appendComponent(PathComponentType.CHILD, name);
+		}
+		
+		public boolean appendRoot() {
+			return this.appendComponent(PathComponentType.ROOT, String.valueOf(symType.root));
+		}
+		
+		public boolean appendParent() {
+			return this.appendComponent(PathComponentType.PARENT, symType.parent);
+		}
+		
+		public PathComponent last() {
+			if (this.isEmpty()) {
+				return null;
+			}
+			return this.get(this.size() - 1);
+		}
+		
+		public String lastChild() {
+			PathComponent last = this.last();
+			if (last != null) {
+				return last.getValue();
+			}
+			return null;
+		}
+		
+		public PathComponent removeLast() {
+			if (this.isEmpty()) {
+				return null;
+			}
+			return this.remove(this.size() - 1);
+		}
+		
+		/**
+		 * @deprecated use
+		 *             {@link PathNavigator#cleanPath(PathComponent.PathComponentsList)}
+		 *             instead
+		 */
+		@Deprecated
+		public void cleanPath() {
+			ListIterator<PathComponent> it = this.listIterator();
+			int lr = this.lastIndexOf(new PathComponent(PathComponentType.ROOT, ""));
+			for (int i = 0; i < lr && it.hasNext(); i++) {
+				it.next();
+				it.remove();
+			}
+			it = this.listIterator();
+			while (it.hasNext()) {
+				PathComponent curr = it.next();
+				if (curr.getType().equals(PathComponentType.PARENT)) {
+					if (it.hasPrevious()) {
+						PathComponent prev = it.previous();
+						if (prev.getType().equals(PathComponentType.CHILD)) {
+							it.remove();
+							it.next();
+							it.remove();
+						}
+					}
+				}
+			}
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder pathBuilder = new StringBuilder();
+			for (PathComponent component : this) {
+				pathBuilder.append(PathComponent.escapeComponent(component.getValue(), this.symType));
+			}
+			return pathBuilder.toString();
+		}
+		
+		@Override
+		public PathComponentsList clone() {
+			PathComponentsList copy = (PathComponentsList) super.clone();
+			copy.symType = this.symType;
+			return copy;
+		}
+	}
+	
+	public static enum PathComponentType {
+		ROOT, PARENT, CHILD;
+	}
 }
--- src/main/java/me/unei/configuration/api/fs/PathNavigator.java
+++ src/main/java/me/unei/configuration/api/fs/PathNavigator.java
@@ -2,256 +2,253 @@ package me.unei.configuration.api.fs;
 
 import java.util.function.Consumer;
 
-import org.apache.commons.lang.NullArgumentException;
-
 import me.unei.configuration.api.fs.PathComponent.PathComponentType;
 import me.unei.configuration.api.fs.PathComponent.PathComponentsList;
 
 public final class PathNavigator<T extends NavigableFile> {
-
-    private PathComponentsList currentPath;
-    private T currentNode;
-
-    public PathNavigator(T rootFile) {
-        this.currentNode = rootFile;
-        this.currentPath = rootFile.getRealListPath();
-        //this.currentPath = PathNavigator.parsePath(rootFile.getCurrentPath(), PathSymbolsType.BUKKIT);
-    }
-
-    @Deprecated
-    public PathNavigator(T rootFile, PathSymbolsType type) {
-        this.currentNode = rootFile;
-        this.currentPath = PathNavigator.parsePath(rootFile.getCurrentPath(), type);
-    }
-
-    @SuppressWarnings("unchecked")
-    private T getChecked(NavigableFile file) {
-        try {
-            return (T) file;
-        } catch (ClassCastException e) {
-            return null;
-        }
-    }
-
-    public void goToRoot() {
-        this.currentPath.clear();
-        this.currentNode = getChecked(this.currentNode.getRoot());
-    }
-
-    public void goToParent() {
-        if (!this.currentPath.isEmpty()) {
-            this.currentPath.remove(this.currentPath.size() - 1);
-            this.currentNode = getChecked(this.currentNode.getParent());
-        }
-    }
-
-    public void goToChild(String name) {
-        if (name == null || name.isEmpty()) {
-            return;
-        }
-        this.currentPath.appendChild(name);
-        this.currentNode = getChecked(this.currentNode.getChild(name));
-    }
-
-    public String getCurrentPath() {
-        return currentPath.toString();
-    }
-
-    public T getCurrentNode() {
-        return this.currentNode;
-    }
-    
-    public void followAndApply(PathComponentsList path, Consumer<T> action)
-    {
-    	if (action == null) {
-    		throw new NullArgumentException("action");
-    	}
-    	
-    	action.accept(this.currentNode);
-    	
-    	for (PathComponent component : path)
-    	{
-    		switch (component.getType())
-    		{
-    			case ROOT:
-    				this.goToRoot();
-    				break;
-    				
-    			case PARENT:
-    				this.goToParent();
-    				break;
-    				
-    			case CHILD:
-    				this.goToChild(component.getValue());
-    				break;
-    		}
+	
+	private PathComponentsList currentPath;
+	private T currentNode;
+	
+	public PathNavigator(T rootFile) {
+		this.currentNode = rootFile;
+		this.currentPath = rootFile.getRealListPath();
+		// this.currentPath = PathNavigator.parsePath(rootFile.getCurrentPath(),
+		// PathSymbolsType.BUKKIT);
+	}
+	
+	@Deprecated
+	public PathNavigator(T rootFile, PathSymbolsType type) {
+		this.currentNode = rootFile;
+		this.currentPath = PathNavigator.parsePath(rootFile.getCurrentPath(), type);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private T getChecked(NavigableFile file) {
+		try {
+			return (T) file;
+		} catch (ClassCastException e) {
+			return null;
+		}
+	}
+	
+	public void goToRoot() {
+		this.currentPath.clear();
+		this.currentNode = getChecked(this.currentNode.getRoot());
+	}
+	
+	public void goToParent() {
+		if (!this.currentPath.isEmpty()) {
+			this.currentPath.remove(this.currentPath.size() - 1);
+			this.currentNode = getChecked(this.currentNode.getParent());
+		}
+	}
+	
+	public void goToChild(String name) {
+		if (name == null || name.isEmpty()) {
+			return;
+		}
+		this.currentPath.appendChild(name);
+		this.currentNode = getChecked(this.currentNode.getChild(name));
+	}
+	
+	public String getCurrentPath() {
+		return currentPath.toString();
+	}
+	
+	public T getCurrentNode() {
+		return this.currentNode;
+	}
+	
+	public void followAndApply(PathComponentsList path, Consumer<T> action) {
+		if (action == null) {
+			throw new NullPointerException("action must not be null.");
+		}
+		
+		action.accept(this.currentNode);
+		
+		for (PathComponent component : path) {
+			switch (component.getType()) {
+				case ROOT:
+					this.goToRoot();
+					break;
+				
+				case PARENT:
+					this.goToParent();
+					break;
+				
+				case CHILD:
+					this.goToChild(component.getValue());
+					break;
+			}
 			action.accept(this.currentNode);
-    	}
-    }
-
-    public static PathComponentsList parsePath(String path, PathSymbolsType type) {
-        if (path == null || path.isEmpty()) {
-            return new PathComponentsList(type);
-        }
-
-        PathComponentsList components = new PathComponentsList(type);
-        StringBuilder lastComponent = new StringBuilder();
-
-        int i = 0;
-
-        if (PathNavigator.isAbsolute(path, type)) {
-            components.appendRoot();
-            i = 1;
-        }
-
-        boolean escaped = false;
-        for (; i < path.length(); i++) {
-            char c = path.charAt(i);
-
-            if (escaped) {
-                lastComponent.append(c);
-                escaped = false;
-                continue;
-            }
-
-            if (c == type.escape) {
-                escaped = true;
-            } else if (PathNavigator.hasParentChar(path, i, type)) {
-                if (lastComponent.length() > 0) {
-                    components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
-                    lastComponent.setLength(0);
-                }
-                components.appendParent();
-                i++;
-            } else if (PathNavigator.hasSeperatorChar(path, i, type)) {
-                if (lastComponent.length() > 0) {
-                    components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
-                    lastComponent.setLength(0);
-                }
-            } else {
-                lastComponent.append(c);
-            }
-        }
-
-        if (lastComponent.length() > 0) {
-            components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
-        }
-
-        return components;
-    }
-
-    public static PathComponentsList cleanPath(PathComponentsList path) {
-        PathComponentsList cleanPath = new PathComponentsList(path.getSymbolsType());
-        for (PathComponent component : path) {
-            switch(component.getType()) {
-                case ROOT:
-                    cleanPath.clear();
-                    cleanPath.add(component);
-                    break;
-
-                case PARENT:
-                    if (!cleanPath.isEmpty() && cleanPath.get(cleanPath.size() - 1).getType().equals(PathComponentType.CHILD)) {
-                        cleanPath.remove(cleanPath.size() - 1);
-                    } else {
-                        cleanPath.add(component);
-                    }
-                    break;
-
-                case CHILD:
-                    cleanPath.add(component);
-                    break;
-            }
-        }
-        return cleanPath;
-    }
-
-    public boolean followPath(PathComponentsList path) {
-        if (path == null) {
-            return false;
-        }
-
-        for (PathComponent component : path) {
-            if (component == null) {
-                return false;
-            }
-
-            switch(component.getType()) {
-                case ROOT:
-                    this.goToRoot();
-                    break;
-
-                case PARENT:
-                    this.goToParent();
-                    break;
-
-                case CHILD:
-                    this.goToChild(component.getValue());
-                    break;
-            }
-        }
-        return true;
-    }
-
-    public boolean navigate(String path, PathSymbolsType type) {
-        return followPath(PathNavigator.parsePath(path, type));
-    }
-
-    private static boolean isAbsolute(String path, PathSymbolsType type) {
-        if (path == null || path.isEmpty()) {
-            return false;
-        }
-        if (path.charAt(0) != type.root) {
-            return false;
-        }
-        if (path.length() > 1) {
-            if (PathNavigator.hasParentChar(path, 0, type)) {
-                return false;
-            }
-            if (path.charAt(1) != type.root) {
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean hasParentChar(String path, int index, PathSymbolsType type) {
-        return path.startsWith(type.parent, index);
-    }
-
-    private static boolean hasSeperatorChar(String path, int index, PathSymbolsType type) {
-        if (index < 0 || index >= path.length()) {
-            return false;
-        }
-        return path.charAt(index) == type.separator;
-    }
-
-    public static enum PathSymbolsType {
-        BUKKIT('\\', '.', '.', ".."), UNIX('\\', '/', '/', "..");
-
-        public final char escape;
-        public final char separator;
-        public final char root;
-        public final String parent;
-
-        private PathSymbolsType(char p_escape, char p_separator, char p_root, String p_parent) {
-            this.escape = p_escape;
-            this.separator = p_separator;
-            this.root = p_root;
-            this.parent = p_parent;
-        }
-
-        @Deprecated
-        public static PathSymbolsType tryDetectType(String apath) {
-            int li = apath.lastIndexOf(UNIX.parent);
-            if (li >= 0) {
-                if (li > 0 && apath.charAt(li - 1) == UNIX.separator) {
-                    return PathSymbolsType.UNIX;
-                } else if ((li + 2) < apath.length() && apath.charAt(li + 2) == UNIX.separator) {
-                    return PathSymbolsType.UNIX;
-                }
-            }
-            return PathSymbolsType.BUKKIT;
-        }
-    }
+		}
+	}
+	
+	public static PathComponentsList parsePath(String path, PathSymbolsType type) {
+		if (path == null || path.isEmpty()) {
+			return new PathComponentsList(type);
+		}
+		
+		PathComponentsList components = new PathComponentsList(type);
+		StringBuilder lastComponent = new StringBuilder();
+		
+		int i = 0;
+		
+		if (PathNavigator.isAbsolute(path, type)) {
+			components.appendRoot();
+			i = 1;
+		}
+		
+		boolean escaped = false;
+		for (; i < path.length(); i++) {
+			char c = path.charAt(i);
+			
+			if (escaped) {
+				lastComponent.append(c);
+				escaped = false;
+				continue;
+			}
+			
+			if (c == type.escape) {
+				escaped = true;
+			} else if (PathNavigator.hasParentChar(path, i, type)) {
+				if (lastComponent.length() > 0) {
+					components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
+					lastComponent.setLength(0);
+				}
+				components.appendParent();
+				i++;
+			} else if (PathNavigator.hasSeperatorChar(path, i, type)) {
+				if (lastComponent.length() > 0) {
+					components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
+					lastComponent.setLength(0);
+				}
+			} else {
+				lastComponent.append(c);
+			}
+		}
+		
+		if (lastComponent.length() > 0) {
+			components.appendComponent(PathComponentType.CHILD, lastComponent.toString());
+		}
+		
+		return components;
+	}
+	
+	public static PathComponentsList cleanPath(PathComponentsList path) {
+		PathComponentsList cleanPath = new PathComponentsList(path.getSymbolsType());
+		for (PathComponent component : path) {
+			switch (component.getType()) {
+				case ROOT:
+					cleanPath.clear();
+					cleanPath.add(component);
+					break;
+				
+				case PARENT:
+					if (!cleanPath.isEmpty()
+							&& cleanPath.get(cleanPath.size() - 1).getType().equals(PathComponentType.CHILD)) {
+						cleanPath.remove(cleanPath.size() - 1);
+					} else {
+						cleanPath.add(component);
+					}
+					break;
+				
+				case CHILD:
+					cleanPath.add(component);
+					break;
+			}
+		}
+		return cleanPath;
+	}
+	
+	public boolean followPath(PathComponentsList path) {
+		if (path == null) {
+			return false;
+		}
+		
+		for (PathComponent component : path) {
+			if (component == null) {
+				return false;
+			}
+			
+			switch (component.getType()) {
+				case ROOT:
+					this.goToRoot();
+					break;
+				
+				case PARENT:
+					this.goToParent();
+					break;
+				
+				case CHILD:
+					this.goToChild(component.getValue());
+					break;
+			}
+		}
+		return true;
+	}
+	
+	public boolean navigate(String path, PathSymbolsType type) {
+		return followPath(PathNavigator.parsePath(path, type));
+	}
+	
+	private static boolean isAbsolute(String path, PathSymbolsType type) {
+		if (path == null || path.isEmpty()) {
+			return false;
+		}
+		if (path.charAt(0) != type.root) {
+			return false;
+		}
+		if (path.length() > 1) {
+			if (PathNavigator.hasParentChar(path, 0, type)) {
+				return false;
+			}
+			if (path.charAt(1) != type.root) {
+				return true;
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	private static boolean hasParentChar(String path, int index, PathSymbolsType type) {
+		return path.startsWith(type.parent, index);
+	}
+	
+	private static boolean hasSeperatorChar(String path, int index, PathSymbolsType type) {
+		if (index < 0 || index >= path.length()) {
+			return false;
+		}
+		return path.charAt(index) == type.separator;
+	}
+	
+	public static enum PathSymbolsType {
+		BUKKIT('\\', '.', '.', ".."), UNIX('\\', '/', '/', "..");
+		
+		public final char escape;
+		public final char separator;
+		public final char root;
+		public final String parent;
+		
+		private PathSymbolsType(char p_escape, char p_separator, char p_root, String p_parent) {
+			this.escape = p_escape;
+			this.separator = p_separator;
+			this.root = p_root;
+			this.parent = p_parent;
+		}
+		
+		@Deprecated
+		public static PathSymbolsType tryDetectType(String apath) {
+			int li = apath.lastIndexOf(UNIX.parent);
+			if (li >= 0) {
+				if (li > 0 && apath.charAt(li - 1) == UNIX.separator) {
+					return PathSymbolsType.UNIX;
+				} else if ((li + 2) < apath.length() && apath.charAt(li + 2) == UNIX.separator) {
+					return PathSymbolsType.UNIX;
+				}
+			}
+			return PathSymbolsType.BUKKIT;
+		}
+	}
 }
